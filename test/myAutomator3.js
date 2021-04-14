5ffd92adce445a9e101b2c73cea3efed048fa1a8
zzliux <416918875@qq.com>
2021-04-15 01:37:56 +0800

    myshell模块调试

--- test/myAutomator3.js
+++ test/myAutomator3.js
@@ -6,19 +6,41 @@ events.on('exit', function () {
 // new myAutomator(sh);
 // const eventDeviceStr = sh.execAndWaitFor('getevent -p');
 // files.write('./eventDeviceStr', eventDeviceStr);
-execPromise(sh, 'getevent -p', 2000).then(function (eventDeviceStr) {
+execPromise(sh, 'getevent -p', 200).then(function (eventDeviceStr) {
     files.write('./eventDeviceStr', eventDeviceStr);
     const reg = /add device.+?:\s+(.+)\r?\n\s+name:\s+"(.+?)"/g;
     const r = eventDeviceStr.match(reg);
     console.log(r);
 });
 
+// let arr1 = ['su', 'aosp:/ $ su'];
+// let arr2 = ['su', 'aosp:/ $ su', 'getevent -p'];
+function mixinRepeatKeywords(arr1, arr2) {
+    let arrIndex = 0;
+    for (let i = 1; i <= arr1.length && i <= arr2.length; i++) {
+        let sub1 = arr1.slice(arr1.length - i, arr1.length);
+        let sub2 = arr2.slice(0, i);
 
-
+        let flag = true;
+        for (let j = 0; j < sub1.length; j++) {
+            if (sub1[j] !== sub2[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            arrIndex = i;
+            break;
+        }
+    }
+    return arr1.concat(arr2.slice(arrIndex, arr2.length));
+}
+// mixinRepeatKeywords(arr1, arr2);
 
 function execPromise(sh, cmd, overTime) {
     
     let outPutArr = [];
+    let outPutArr2 = [];
     let lastOutPutDate = null;
     let ret = new Promise(function (resolve, reject) {
         // 计时器，超过overTime毫秒没有输出则计为已返回所有数据
@@ -26,6 +48,7 @@ function execPromise(sh, cmd, overTime) {
             if (lastOutPutDate && new Date().getTime() - lastOutPutDate.getTime() > overTime) {
                 clearInterval(timmer);
                 sh.setCallback({ onNewLine: function () { }});
+                files.write('./eventDeviceStrr', JSON.stringify(outPutArr2));
                 resolve(outPutArr.join('\n'));
             }
         }, 100)
@@ -33,12 +56,15 @@ function execPromise(sh, cmd, overTime) {
             onNewLine: function (line) {
                 lastOutPutDate = new Date();
                 const arr = line.split(/\r?\n/);
-                const index = outPutArr.lastIndexOf(arr[0]);
-                if (-1 !== index) {
-                    outPutArr = outPutArr.slice(index, outPutArr.length - index).concat(arr);
-                } else {
-                    outPutArr = outPutArr.concat(arr);
-                }
+                outPutArr = mixinRepeatKeywords(outPutArr, arr);
+                outPutArr2.push(arr);
+
+                // const index = outPutArr.lastIndexOf(arr[0]);
+                // if (-1 !== index) {
+                //     outPutArr = outPutArr.slice(index, outPutArr.length - index).concat(arr);
+                // } else {
+                //     outPutArr = outPutArr.concat(arr);
+                // }
             },
             // onOutput: function (str) {
             //     if (/( # | \$ )$/.test(str)) {
--- test/sh.js
+++ test/sh.js
@@ -1,3 +1,83 @@
+/**
+ * 
+ * @param {String} flag 'su'|'sh'
+ */
+function myShell (flag) {
+    let self = this;
+    this.process = java.lang.Runtime.getRuntime().exec(flag);
+    this.writer = new java.io.BufferedWriter(new java.io.OutputStreamWriter(this.process.getOutputStream()));
+    this.successReader = new java.io.BufferedReader(new java.io.InputStreamReader(this.process.getInputStream()));
+    this.errorReader = new java.io.BufferedReader(new java.io.InputStreamReader(this.process.getErrorStream()));
+    this.successMsgDate = new Date();
+    this.successMsg = '';
+    this.execLock = threads.lock();
+    // this.execLock = new java.util.concurrent.locks.ReentrantLock();
+    threads.start(function () {
+        let s = null;
+        while (true) {
+            s = self.successReader.readLine(); // 这个会阻塞
+            // console.log(s);
+            successMsgDate = new Date();
+            self.successMsg += s + '\n';
+        }
+    });
+}
 
-let sh = new Packages.jackpal.androidterm.ShellTermSession(context.getResources(), android.preference.PreferenceManager.getDefaultSharedPreferences(context))
-console.log(sh);
+/**
+ * 
+ * @param {*} command 
+ * @param {*} overtime 个人测试建议大于200ms
+ * @param {*} callback 
+ */
+myShell.prototype.exec = function (command, overtime, callback) {
+    if (typeof callback !== 'function') {
+        callback = function () {}
+    }
+    if (typeof overtime === 'function') {
+        callback = overtime;
+        overtime = 200;
+    } else {
+        overtime = overtime || 200;
+    }
+    let self = this;
+    threads.start(function () {
+        console.log(command + ':' + self.execLock);
+        self.execLock.lock();
+        self.writer.write(command);
+        self.writer.newLine();
+        self.writer.flush();
+        self.successMsgDate = new Date();
+        let timmer = setInterval(function () {
+            console.log(command + ':self.successMsgDate.getTime(): ' + self.successMsgDate.getTime());
+            if (new Date().getTime() - self.successMsgDate.getTime() > overtime) {
+                clearInterval(timmer);
+                let result = self.successMsg
+                self.successMsg = '';
+                self.execLock.unlock();
+                callback(result);
+            }
+        }, 50);
+    });
+}
+
+
+if (typeof module == 'undefined') {
+    module = {};
+    const sh = new myShell('su');
+
+    sleep(1000);
+
+    sh.exec('getevent -p', function (eventDeviceStr) {
+        console.info('getevent -p');
+        files.write('eventDeviceStr', eventDeviceStr);
+    });
+    
+    sh.exec('ls -an', function (lsanstr) {
+        console.info('ls -an');
+        files.write('lsanstr', lsanstr)
+    });
+    // sleep(1000);
+}
+
+
+module.exports = myShell;
