ccebecec0a2b210a4ba066ff8a79ec9565f6d6f3
zzliux <416918875@qq.com>
2022-01-04 19:21:39 +0800

    更新readme

--- run.js
+++ run.js
@@ -1,7 +1,11 @@
 // let path = '/sdcard/Android/data/' + context.getPackageName() + '/assttyys_ng';
 let path = context.getExternalFilesDir(null).getAbsolutePath() + '/assttyus_ng';
 toastLog("正在加载程序，请稍后");
-let r = http.get('https://gitee.com/zzliux/assttyys_autojs/raw/ng_dev/assttyys_ng.zip');
+let r = http.get('https://gitee.com/zzliux/assttyys_autojs/raw/ng_dev/assttyys_ng.zip', {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.59'
+    }
+});
 files.ensureDir(path + '/assttyys_ng.zip');
 files.writeBytes(path + '/assttyys_ng.zip', r.body.bytes());
 files.removeDir(path + '/assttyys_ng');
