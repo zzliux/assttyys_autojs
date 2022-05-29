d8cd62b38527fe11386cad45dccd820ab62ce2b5
zzliux <zzliux@outlook.com>
2022-05-29 19:52:11 +0800

    新增：秘闻_挑战支持妖刀姬竞速

--- src/common/version.js
+++ src/common/version.js
@@ -193,6 +193,9 @@ export const versionList = [{
 }, {
     version: '20220529_01',
     desc: '新增：活动功能战场巡逻杂项，目前功能为终焉决战分数大于5500w分时自动退出，请手动开启'
+}, {
+    version: '20220529_02',
+    desc: '新增：秘闻_挑战支持妖刀姬竞速'
 }];
 
 export default versionList[versionList.length - 1].version;
