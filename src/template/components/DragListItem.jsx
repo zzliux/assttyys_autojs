6c76329ae749589ebf318fea96853a5c82595da6
zzliux <zzliux@outlook.com>
2022-06-02 15:58:31 +0800

    DragListItem封装

--- src/template/components/DragListItem.jsx
+++ src/template/components/DragListItem.jsx
@@ -2,21 +2,57 @@ import React from 'react';
 import ListItemButton from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
+import { Draggable } from 'react-beautiful-dnd';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
 
 /**
  * TODO
  */
 export default (prop) => {
+
+    console.log(typeof prop.draggableId);
+
+    const getItemStyle = (isDragging, draggableStyle) => {
+        return {
+            // some basic styles to make the items look a bit nicer
+            userSelect: "none",
+            // padding: grid * 2,
+            // margin: `0 0 ${grid}px 0`,
+
+            // change background colour if dragging
+            background: isDragging ? "#efefef" : "#fff",
+
+            // styles we need to apply on draggables
+            ...draggableStyle
+        }
+    };
+
     return (
-        <div style={{ color: 'rgba(0, 0, 0, .7)' }}>
-            <ListItemButton>
-                {/* <ListItemIcon>
+        <Draggable draggableId={`${prop.index}`} index={prop.index}>
+            {(provided, snapshot) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    style={{
+                        color: 'rgba(0, 0, 0, .7)', ...getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                        )
+                    }}
+                >
+                    <ListItemButton>
+                        {/* <ListItemIcon>
                             <WifiIcon />
                         </ListItemIcon> */}
-                <ListItemText primary={prop.text} />
-                {prop.children}
-            </ListItemButton>
-            <Divider variant="fullWidth" component="li" />
-        </div>
+                        <ListItemText primary={prop.text} />
+                        <div {...provided.dragHandleProps}>
+                            <DragHandleIcon sx={{ mr: '20px' }} />
+                        </div>
+                        {prop.children}
+                    </ListItemButton>
+                    <Divider variant="fullWidth" component="li" />
+                </div>
+            )}
+        </Draggable>
     );
 }
--- src/template/pages/SchemeList.jsx
+++ src/template/pages/SchemeList.jsx
@@ -4,13 +4,12 @@ import List from '@mui/material/List';
 import ListSubheader from '@mui/material/ListSubheader';
 import Divider from '@mui/material/Divider';
 import AppBar from '../components/AppBar';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import DragListItem from '../components/DragListItem';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
-import DragHandleIcon from '@mui/icons-material/DragHandle';
 
 export default () => {
     const navigate = useNavigate();
@@ -55,21 +54,6 @@ export default () => {
         return result;
     };
 
-    const getItemStyle = (isDragging, draggableStyle) => {
-        return {
-            // some basic styles to make the items look a bit nicer
-            userSelect: "none",
-            // padding: grid * 2,
-            // margin: `0 0 ${grid}px 0`,
-
-            // change background colour if dragging
-            background: isDragging ? "#efefef" : "#fff",
-
-            // styles we need to apply on draggables
-            ...draggableStyle
-        }
-    };
-
     React.useEffect(() => {
         (async () => {
             const newDataList = await AutoWeb.autoPromise('getSchemeList');
@@ -100,30 +84,15 @@ export default () => {
                             >
                                 <Divider variant="fullWidth" component="li" />
                                 {dataList.map((item, index) => (
-                                    <Draggable key={`item-${index}`} draggableId={`${index}`} index={index}>
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                style={getItemStyle(
-                                                    snapshot.isDragging,
-                                                    provided.draggableProps.style
-                                                )}
-                                            >
-                                                <DragListItem
-                                                    text={item.schemeName}
-                                                    draggableId={`${index}`}
-                                                >
-                                                    <div {...provided.dragHandleProps}>
-                                                        <DragHandleIcon sx={{ mr: '20px' }} />
-                                                    </div>
-                                                    <div onClick={handleStar(index)} >
-                                                        {item.star ? <StarRoundedIcon sx={{ mr: '10px', color: '#1976d2' }} /> : <StarBorderRoundedIcon sx={{ mr: '10px' }} />}
-                                                    </div>
-                                                </DragListItem>
-                                            </div>
-                                        )}
-                                    </Draggable>
+                                    <DragListItem
+                                        key={`item-${index}`}
+                                        text={item.schemeName}
+                                        index={index}
+                                    >
+                                        <div onClick={handleStar(index)} >
+                                            {item.star ? <StarRoundedIcon sx={{ mr: '10px', color: '#1976d2' }} /> : <StarBorderRoundedIcon sx={{ mr: '10px' }} />}
+                                        </div>
+                                    </DragListItem>
                                 ))}
                                 {provided.placeholder}
                             </List>
