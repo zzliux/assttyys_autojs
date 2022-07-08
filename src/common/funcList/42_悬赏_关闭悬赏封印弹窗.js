009ea50ba8765ff7173b90354257a410786b7a92
liuwei <1323768301@qq.com>
2022-07-08 10:35:07 +0800

    M 悬赏前往副本逻辑修改

---
+++ src/common/funcList/42_悬赏_关闭悬赏封印弹窗.js
@@ -0,0 +1,34 @@
+import {
+	setCurrentScheme
+} from '@/common/tool';
+
+const normal = -1; //定义常量
+const left = 0;
+const center = 1;
+const right = 2;
+
+export default {
+	id: 42,
+	name: '悬赏_关闭悬赏封印弹窗',
+	desc: '请打开悬赏封印弹窗后开启脚本',
+	checked: false,
+	operator: [{
+		desc: [1280, 720,
+			[
+				[center, 644, 40, 0x544321],
+				[center, 638, 52, 0x75542a],
+				[left, 95, 124, 0x7b7372],
+				[left, 105, 90, 0x634724],
+				[right, 1161, 85, 0x553c1f],
+				[right, 1097, 582, 0xffeeaa],
+				[right, 1110, 580, 0xf9eba8],
+				[right, 1107, 611, 0xf4e39a],
+				[right, 1128, 606, 0xe5cf89],
+				[right, 1122, 584, 0xf4e3a4]
+			]
+		],
+		oper: [
+			[left, 1280, 720, 1164, 116, 1189, 143, 500],
+		]
+	}]
+}
