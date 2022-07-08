7e6f80ea7541ba9703cf2146df2f8f7864d8875b
liuwei <1323768301@qq.com>
2022-07-08 10:35:18 +0800

    M 庭院进入探索副本取色修改

--- src/common/funcList/029_庭院进入探索地图.js
+++ src/common/funcList/029_庭院进入探索地图.js
@@ -8,17 +8,18 @@ export default {
 	name: '庭院进入探索地图',
 	desc: '请使用默认庭院皮肤，启用该功能后在庭院下会自动进入探索地图界面',
 	operator: [{
-		desc: [1280,720,
-			[[center,229,30,0x614040],
-			[center,404,36,0xfbcb78],
-			[center,437,79,0xee7a59],
-			[center,1151,53,0xcba574],
-			[center,1237,54,0xceac79],
-			[right,1235,610,0x5c7575],
-			[center,1020,597,0xc73e4c],
-			[center,635,608,0xf4f4f3],
-			[center,373,623,0xe2dfdc],
-			[center,746,630,0xd9ceca]]
+		desc: [1280, 720,
+			[
+				[center, 403, 39, 0xfbc975],
+				[center, 439, 76, 0xf27c5d],
+				[right, 1062, 55, 0xcca173],
+				[right, 1152, 54, 0xcda170],
+				[right, 1236, 50, 0xcba274],
+				[right, 1222, 607, 0xefda9f],
+				[right, 1226, 638, 0xdfd0c8],
+				[right, 1128, 616, 0xe8f9ff],
+				[right, 1102, 621, 0x98b9db]
+			]
 		],
 		oper: [
 			[left, 1280, 720, 0, 0, 32, 63, 1000]
@@ -26,12 +27,16 @@ export default {
 	}],
 	operatorFunc(thisScript, thisOperator) {
 		if (thisScript.oper({
-			name: '庭院判断',
-			operator: [{ desc: thisOperator[0].desc }]
-		})) {
+				name: '庭院判断',
+				operator: [{
+					desc: thisOperator[0].desc
+				}]
+			})) {
 			let point = thisScript.findMultiColor('庭院_探索灯笼');
 			if (point) {
-				let oper = [[point.x, point.y, point.x + thisOperator[0].oper[0][2], point.y + thisOperator[0].oper[0][3], thisOperator[0].oper[0][4]]];
+				let oper = [
+					[point.x, point.y, point.x + thisOperator[0].oper[0][2], point.y + thisOperator[0].oper[0][3], thisOperator[0].oper[0][4]]
+				];
 				thisScript.helperBridge.regionClick(oper, thisScript.scheme.commonConfig.afterClickDelayRandom);
 				return true;
 			} else {
