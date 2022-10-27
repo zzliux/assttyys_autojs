e234e80349281919c4aad36d48495c322f6d52ff
zzliux <zzliux@outlook.com>
2022-10-27 20:48:44 +0800

    update nomalDistributionGenerator.js

--- test/nomalDistributionGenerator.js
+++ test/nomalDistributionGenerator.js
@@ -1,22 +1,55 @@
-let xs = [];
-let ys = [];
+let data = [];
 
-let cnt = 1000;
+let cnt = 2000;
 while (cnt--) {
     // TODO 需要根据坐标范围和用户的androidid hash出一个中值点
     // TODO 根据用户的androidid hash出一个标准差
+    // [69, 171, 170, 452]
+    // x: 69~170
+    // y: 171~452
+    data.push([
+        getRndBias(69, 170, 113, 1),
+        getRndBias(171, 452, 206, 1),
+    ]);
 }
 
+console.log(JSON.stringify(data));
 
-// 因此，假如我们要获得均值为180，要68.26%左右的NPC身高都在[170,190]之内
-// 即1个标准差范围内，因此标准差为10, 可以通过getNumberInNormalDistribution(180,10) 调用
-function getNumberInNormalDistribution(mean, std_dev) {
-    var u = 0.0, v = 0.0, w = 0.0, c = 0.0;
-    do {
-        u = Math.random() * 2 - 1.0;
-        v = Math.random() * 2 - 1.0;
-        w = u * u + v * v;
-    } while (w == 0.0 || w >= 1.0)
-    c = Math.sqrt((-2 * Math.log(w)) / w);
-    return mean + (u * c * std_dev);
+/**
+ * 
+ * @param {*} min 最小值
+ * @param {*} max 最大值
+ * @param {*} bias 偏向值
+ * @param {*} influence 影响力 [0.0, 1.0]
+ * @returns 
+ */
+function getRndBias(min, max, bias, influence) {
+    var rnd = Math.random() * (max - min) + min,   // random in range
+        mix = Math.random() * influence;           // random mixer
+    return parseInt(rnd * (1 - mix) + bias * mix);           // mix full range and bias
+}
+
+/**
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ * @param {*} N 
+ * @param {*} D 
+ * @returns 
+ */
+function getRndBias2(min, max, N, D) {
+    var a = 1,
+        b = 50,
+        c = D * 100;
+  
+    var influence = Math.floor(Math.random() * (101)),
+      x = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+    return x > N 
+           ? x + Math.floor(gauss(influence) * (N - x)) 
+           : x - Math.floor(gauss(influence) * (x - N));
+  
+    function gauss(x) {
+      return a * Math.exp(-(x - b) * (x - b) / (2 * c * c));
+    }
 }
