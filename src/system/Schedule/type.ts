8da59fb0d78a77db6876e6dd0fe2f932a707350a
zzliux <zzliux@outlook.com>
2023-05-22 11:35:00 +0800

    调度中心重构timer

--- src/system/Schedule/type.ts
+++ src/system/Schedule/type.ts
@@ -1,3 +1,9 @@
 export type RepeatModeType = 0 | 1 | 2 | 3;
 
-export type StatusType = 'waiting' | 'running' | 'done';
+/**
+ * waiting: 啥也没干
+ * queueing: 队列中，等待执行
+ * running: 执行中
+ * done: 运行完成
+ */
+export type StatusType = 'waiting'| 'queueing' | 'running' | 'done';
