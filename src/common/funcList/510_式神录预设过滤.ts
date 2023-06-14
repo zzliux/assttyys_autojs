f286af56046f38d7fe98d20813c6babcbf55d93e
kk <810518262@qq.com>
2023-06-15 02:17:42 +0800

    1

--- src/common/funcList/510_式神录预设过滤.ts
+++ src/common/funcList/510_式神录预设过滤.ts
@@ -217,7 +217,6 @@ export class Func510 implements IFuncOrigin {
                         ]];
                         thisScript.helperBridge.regionClick(oper, thisScript.scheme.commonConfig.afterClickDelayRandom);
                         thisScript.global.change_shikigami_state = 'change_team_default_1';
-                        // return true;
                     } else if (thisScript.global.change_shikigami_state.includes('change_team_default')) {
                         console.log(`式神录_当前选中队伍预设`);
                         let trueDefaultNum = null;
@@ -235,11 +234,9 @@ export class Func510 implements IFuncOrigin {
                         }
 
                         thisScript.helperBridge.regionClick([oper], thisScript.scheme.commonConfig.afterClickDelayRandom);
-                        // return true;
                     } else if (thisScript.global.change_shikigami_state === 'finish') {
                         thisScript.global.preset_once_groupNum = null;
                         thisScript.global.preset_once_defaultNum = null;
-
                         return thisScript.oper({
                             name: '退出式神录',
                             operator: [{
