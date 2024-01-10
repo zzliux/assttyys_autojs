89e3f3b7d660c054a5415d322924960d2fa2f60d
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2024-01-10 14:55:30 +0800

    修改经验妖怪方案

--- dist/web.js
+++ dist/web.js
@@ -58664,16 +58664,13 @@ var SchemeList = [
         id: 2,
         schemeName: '【日常】开启经验BUFF_经验妖怪',
         groupName: '日常',
-        list: [503, 509, 510, 511, 501, 50],
+        list: [509, 510, 511, 501, 50],
         config: {
             '50': {
                 scheme_switch_enabled: true,
                 next_scheme: '【日常】经验妖怪',
                 buff_type: '经验',
                 ready_once_buff: false,
-            },
-            '503': {
-                afterCountOper: '不进行任何操作',
             },
             '510': {
                 'groupName': '默认分组',
