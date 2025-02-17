298d2518f2f2ac06246a572f80c4908c3f37ac72
shui670 <810518262@qq.com>
2025-02-18 05:28:34 +0800

    优化311红标，510式神增加第二套预设 315剥离开关内容（尝试其他功能换预设）

--- src/common/funcList/510_式神录预设过滤.ts
+++ src/common/funcList/510_式神录预设过滤.ts
@@ -30,16 +30,28 @@ export class Func510 implements IFuncOrigin {
 			default: false,
 		}, {
 			name: 'groupNum',
-			desc: '目标预设的分组在第N个（输入数字，只支持8个以内）',
+			desc: '第一套目标预设的分组在第N个（输入数字，只支持8个以内）',
 			type: 'text',
 			default: '1',
 			value: '1',
 		}, {
 			name: 'defaultNum',
-			desc: '目标预设在第N个（输入数字，只支持4个以内）',
+			desc: '第一套目标预设在第N个（输入数字，只支持前4个）',
 			type: 'text',
 			default: '1',
 			value: '1',
+		}, {
+			name: 'groupNum2',
+			desc: '第二套目标预设的分组在第N个（输入数字，只支持8个以内）',
+			type: 'text',
+			default: '0',
+			value: '0',
+		}, {
+			name: 'defaultNum2',
+			desc: '第二套目标预设在第N个（输入数字，只支持4个以内）',
+			type: 'text',
+			default: '0',
+			value: '0',
 		}],
 	}];
 	operator: IFuncOperatorOrigin[] = [
@@ -224,6 +236,32 @@ export class Func510 implements IFuncOrigin {
 						//  需要点击两次
 						if (thisScript.global.change_shikigami_state === 'change_team_default_1') {
 							thisScript.global.change_shikigami_state = 'change_team_default_2';
+						} else if (thisConf.groupNum2 != '0') {
+							thisScript.global.change_shikigami_state = 'second_groud';
+						} else {
+							thisScript.global.change_shikigami_state = 'finish';
+						}
+						thisScript.regionClick([oper]);
+						return true;
+					} else if (thisScript.global.change_shikigami_state === 'second_groud') {
+						const tureGroupNum = Number(thisConf.groupNum2) - 1;
+						const oper = [[
+							thisOperator[7].oper[0][0],
+							thisOperator[7].oper[0][1] + (thisOperator[7].oper[0][4] * tureGroupNum),
+							thisOperator[7].oper[0][2],
+							thisOperator[7].oper[0][3] + (thisOperator[7].oper[0][4] * tureGroupNum),
+							500
+						]];
+						thisScript.regionClick(oper);
+						thisScript.global.change_shikigami_state = 'second_default_1';
+						return true;
+					} else if (thisScript.global.change_shikigami_state.includes('second_default')) {
+						console.log('式神录_当前选中队伍预设');
+						const trueDefaultNum = Number(thisConf.defaultNum2) - 1;
+						const oper = thisOperator[8].oper[trueDefaultNum];
+						//  需要点击两次
+						if (thisScript.global.change_shikigami_state === 'second_default_1') {
+							thisScript.global.change_shikigami_state = 'second_default_2';
 						} else {
 							thisScript.global.change_shikigami_state = 'finish';
 						}
