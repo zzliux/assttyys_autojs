7e79c43a923a93f0c9d999f99284e3e3fe02bc0c
zzliux <zzliux@outlook.com>
2025-04-06 01:08:49 +0800

    优化：24获得奖励确认适配只有一排且5个的时候

--- src/common/funcList/024_获得奖励确认.ts
+++ src/common/funcList/024_获得奖励确认.ts
@@ -35,5 +35,20 @@ export class Func024 implements IFuncOrigin {
 		oper: [
 			[left, 1280, 720, 69, 171, 170, 452, 500]
 		]
+	}, { // 奖励一排且只有5个的时候
+		desc: [1280, 720,
+			[
+				[center, 379, 241, 0x38291d],
+				[center, 897, 242, 0x382a1d],
+				[center, 959, 237, 0x5e3d22],
+				[center, 922, 426, 0x88653c],
+				[center, 364, 419, 0x8d693f],
+				[center, 671, 239, 0xeadca5],
+				[center, 863, 435, 0xbaa289],
+			]
+		],
+		oper: [
+			[center, 1280, 720, 1042, 176, 1187, 585, 500],
+		]
 	}]
 }
